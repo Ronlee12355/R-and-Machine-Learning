8ae0aa98820b4f3ed8da95d2b805f878396cebec
Ron Lee <857748998@qq.com>
2016-12-15 15:06:37 +0800

    Update glm_lm.R

--- glm_lm.R
+++ glm_lm.R
@@ -7,3 +7,8 @@ newdata<-data.frame(x=c(3,6,15))
 tes<-predict(lmfit,newdata,interval="predict")
 par(mfrow=c(2,2))
 plot(lmfit)
+
+#多项式回归
+plot(Quartet$x,Quartet$y2)
+fit<-lm(Quartet$y2~Quartet$x+I(Quartet$x^2),data = Quartet)
+summary(fit)
