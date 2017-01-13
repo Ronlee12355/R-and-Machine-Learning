c3cb57879df25e989d6783b66b9fe1ff497c1f8e
Ron Lee <857748998@qq.com>
2017-01-13 16:45:41 +0800

    Update Regression_Tree.R

--- Regression_Tree.R
+++ Regression_Tree.R
@@ -11,3 +11,28 @@ p_wine<-predict(m_wine,wine_test)
 cor(p_wine,wine_test$quality)#预测值和真实值的相关性
 
 MAE<-mean(abs(p_wine - wine_test$quality))#平均绝对误差，查看预测值离真实值有多远
+
+data("diamonds")
+str(diamonds)
+index<-sample(2,ncol(diamonds),replace = T,prob = c(0.9,0.1))
+trainDia<-diamonds[index==1,]
+testDia<-diamonds[index==2,]
+#观察数据的总结情况
+summary(trainDia)
+symnum(cor(trainDia[,-c(2,3,4)]))
+
+#使用图形来观察数据情况
+library(lattice)
+bwplot(depth~color,data=trainDia)
+
+lm_model <- lm(price~.,data=trainDia)
+par(mfrow=c(2,2))
+plot(lm_model)
+
+library(caret)
+cr<-trainControl(method = "repeatedcv",number = 10,repeats = 10)
+rpart_model<-train(price~.,data = trainDia,method = "rpart",trControl =cr,preProcess="scale")
+
+library(rpart)
+model<-rpart(price~.,data = trainDia,cp=0.03359724)
+model_modified<-prune(model,cp=0.2)
