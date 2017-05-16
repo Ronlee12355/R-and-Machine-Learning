1059ae47b337edb256a3a5b6ec30260e644e8b60
Ron Lee <857748998@qq.com>
2017-05-16 22:56:53 +0800

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -23,3 +23,26 @@ ggplot(data = cabbage_exp,mapping = aes(x=Date,y=Weight,fill=Cultivar))+
 
 ggplot(data = BOD,mapping = aes(x=Time,y=demand))+geom_line()
 ggplot(tg,aes(x=dose,y=length,color=supp))+geom_line(linetype="dashed")+geom_point(size=2,shape=21)
+
+house<-read.table("C:/Users/sdj/Desktop/housing.data",header = F)
+library(stringr)
+corrplot(cor(house),order = "AOE",addCoef.col = "grey",method = "color")
+corrplot(cor(house),order = "AOE",addCoef.col = "grey")
+house_clean<-subset(house,select = -c(V7,V3))
+model<-glm(V14~.,data = house_clean[1:405,])
+summary(model)
+ret<-predict(model,house_clean[406:506,])
+
+library(rpart)
+rpart.model<-rpart(V14~.,data = house[1:405,])
+library(rpart.plot)
+rpart.plot(rpart.model,digits = 5)
+rpart.predict<-predict(rpart.model,house[406:506,])
+cor(rpart.predict,house[406:506,]$V14)
+MAE<-mean(abs(rpart.predict-house[406:506,]$V14))
+
+ggplot(data = cabbage_exp,mapping = aes(x=Date,y=Weight,fill=Cultivar))+
+  geom_bar(stat = "identity",width = 0.6,position = position_dodge(0.7))
+
+ggplot(data = cabbage_exp,mapping = aes(x=Date,y=Weight,fill=Cultivar))+
+  geom_bar(stat = "identity")+guides(fill=guide_legend(reverse = T))
