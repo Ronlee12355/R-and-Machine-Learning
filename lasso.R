7fa7104b29039d38eb920a8db1f800b57f3cd40a
Ron Lee <857748998@qq.com>
2019-05-13 21:27:27 +0800

    Update lasso.R

--- lasso.R
+++ lasso.R
@@ -18,3 +18,31 @@ lasso_mod$results$RMSE
 lasso_imp<-varImp(lasso_mod)
 #get the importance of each feature and eliminate some of them
 lasso_imp$importance
+
+df<-iris %>% select_if(is.numeric)
+str(iris)
+index<-sample(2,nrow(iris),replace = T,prob = c(0.7,0.3))
+tr<-df[index==1,];ts<-df[index==2,]
+para<-expand.grid(lambda=seq(0.05,1,0.01),alpha=1)
+lasso_mod <- train(x=tr %>% select(-price), y=tr$price, method='glmnet', 
+                   tuneGrid=para)
+lasso_mod$bestTune
+imp<-varImp(lasso_mod)
+imp$importance
+
+out=c()
+lam<-seq(0.005,100,0.005)
+for(i in 1:length(lam)){
+  set.seed(200)
+  folds<-createFolds(tr$price, k=5)
+  mes<-lapply(folds, function(x){
+    trains<-tr[-x,];test<-tr[x,]
+    model<-glmnet(x=trains %>% select(-price) %>% as.matrix(), y=trains$price,lambda = lam[i])
+    pre<-predict(model,test %>% select(-price) %>% as.matrix())
+    RSS<-(pre-test$price)^2 %>% sum()
+    TSS<-(trains$price-mean(trains$price))^2 %>% sum()
+    return(1-RSS/TSS)
+  })
+  mes<-unlist(mes) %>% mean()
+  out[i]<-mes
+}
